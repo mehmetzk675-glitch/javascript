ae2b04f6e262da225f3b6ad484fdabbb570c7e26
Mehmet Zeki Işıldak <mehmetzk675@gmail.com>
2026-09-05 14:10:50 +0300

    Add files via upload

---
+++ switch-case/i.js
@@ -0,0 +1,26 @@
+//switch-case
+
+
+let sayı=prompt("lütfen 1 ile 5 arasında bir sayı giriniz:");
+
+switch(sayı){
+case "1":
+    console.log("girdiğiniz sayi 1");
+    break;
+case "2":
+    console.log("girdiğiniz sayı 2");
+    break;
+    case "3":
+    console.log("girdiğiniz sayı 3");
+    break;
+case "4":
+    console.log("girdiğiniz sayı 4");
+    break;
+case "5":
+    console.log("girdiğiniz sayı 5");
+    break;
+    default:
+        console.log("lütfen 1 ile 5 arasında bir sayı giriniz!");
+
+}
+
