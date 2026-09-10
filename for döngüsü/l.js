9d45b882c32032b54888c6014c1f3e02e0aaf20d
Mehmet Zeki Işıldak <mehmetzk675@gmail.com>
2026-09-10 13:56:19 +0300

    Add files via upload

---
+++ for döngüsü/l.js
@@ -0,0 +1,15 @@
+/*
+----FOR DÖNGÜSÜ----
+1 den 10 a kadar sayı yazdır
+*/
+
+for (let i=1;i<=10;i++){
+    console.log(i);
+}
+// 1 den 10 a kadar çift olan sayılar
+
+for (let i=1;i<=10;i++){
+    if(i%2==0){console.log(i);
+        
+    }
+}
