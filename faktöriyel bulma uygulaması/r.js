00150954aa17d9598693e5bc4a0ba23a30af5c66
Mehmet Zeki Işıldak <mehmetzk675@gmail.com>
2026-09-15 12:21:17 +0300

    Add files via upload

---
+++ faktöriyel bulma uygulaması/r.js
@@ -0,0 +1,8 @@
+let faktöreyelibulunacaksayi=prompt("lütfen bir sayı giriniz:");
+
+let fact=1;
+
+for (let i=1;i<=faktöreyelibulunacaksayi;i++){
+fact*=i;
+}
+alert(faktöreyelibulunacaksayi+" sayısının faktöriyeli: "+fact);
