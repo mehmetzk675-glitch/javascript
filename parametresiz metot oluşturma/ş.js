aae382bca6dbdb3956608669c71991628e559fe0
Mehmet Zeki Işıldak <mehmetzk675@gmail.com>
2026-09-24 19:11:50 +0300

    Add files via upload

---
+++ parametresiz metot oluşturma/ş.js
@@ -0,0 +1,23 @@
+//parametresiz ve geriye değer döndürmeyen metot oluşturma.
+
+
+/**
+ * 
+ * function yazdır(){
+ * kodlar yazılacak
+ * }
+ * 
+ */
+
+
+function yazdır(){
+console.log("mehmet zeki ışıldak");
+}
+
+yazdır();
+yazdır();
+
+function topla(){
+    console.log(3+7);
+}
+topla();
